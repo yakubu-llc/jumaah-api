11db3524921e9159f043857fa6bd30cef80dfb94
kareem717 <kareem.717@icloud.com>
2024-08-30 11:37:43 -0400

    init main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/danielgtaylor/huma/v2/humacli"
+	"github.com/joho/godotenv"
+)
+
+type Options struct {
+	Port        int    `help:"Port to listen on" short:"p" default:"8080"`
+	DatabaseURL string `help:"Database URL" short:"d"`
+	APIName     string `help:"API Name" short:"n"`
+	APIVersion  string `help:"API Version" short:"v"`
+	BaseURL     string `help:"Base API URL" short:"B"`
+}
+
+func (o *Options) config() {
+	if port, err := strconv.Atoi(os.Getenv("PORT")); err == nil {
+		o.Port = port
+	}
+
+	o.DatabaseURL = os.Getenv("DATABASE_URL")
+	o.APIName = os.Getenv("API_NAME")
+	o.APIVersion = os.Getenv("API_VERSION")
+	o.BaseURL = os.Getenv("BASE_API_URL")
+}
+
+func main() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env.local file")
+	}
+
+	cli := humacli.New(func(hooks humacli.Hooks, opts *Options) {
+		opts.config()
+
+		hooks.OnStart(func() {
+			fmt.Printf("Starting server on port %d...\n", opts.Port)
+		})
+	})
+
+	cli.Run()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module jummah
 
 go 1.23.0
+
+require (
+	github.com/danielgtaylor/huma/v2 v2.22.1
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
